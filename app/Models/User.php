08ef5c45a410a164b274669c06862e0477c47801
Jose <jos.cornejof@gmail.com>
2025-05-13 14:33:01 -0400

    Actualizacion

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,6 +34,7 @@ class User extends Authenticatable
      */
     public function isAdmin(): bool
     {
-        return $this->is_admin;
+        // Forzamos a bool, incluso si el atributo es null:
+        return (bool) $this->is_admin;
     }
 }
