a26e4d4bb7d794a56a524d10743aa36a30b8866f
Jose <jos.cornejof@gmail.com>
2025-05-24 16:34:05 -0400

    vista

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('title', 'Inicio') – Sushi Akila</title>
+    <title>@yield('title', 'Inicio') – Sushi Akila!</title>
     <link rel="icon" href="{{ asset('img/logo/logo-icon.png') }}" type="image/x-icon">
 
     {{-- CSS y JS personalizado --}}
